baa6ba4008b51170b8113772ff134ad3cca0d6e9
j.tara <j.tara@arduino.cc>
2015-08-29 17:16:16 +0200

    create generic sketch without my personal paypal account

--- Arduino Sketch/YunPaypal/YunPaypal.ino
+++ Arduino Sketch/YunPaypal/YunPaypal.ino
@@ -1,13 +1,13 @@
 /*
   GetLatestPayPalPayment
 
-  Demonstrates retrieving the latest payment to a PayPal account. 
-  
+  Demonstrates retrieving the latest payment to a PayPal account.
+
   This example code is in the public domain.
 */
 
-
-#define TEMBOO_ACCOUNT "jjtara"  // your Temboo account name 
+//this is my personal Temboo acoount so don't worry about it.
+#define TEMBOO_ACCOUNT "jjtara"  // your Temboo account name
 #define TEMBOO_APP_KEY_NAME "myFirstApp"  // your Temboo app key name
 #define TEMBOO_APP_KEY  "88681f56ac9041e6aab886fa2b7624d3"  // your Temboo app key
 
@@ -21,10 +21,10 @@
 // use #define statements to specify these values in a .h file.
 
 // your PayPal app's Client ID, available from developer.paypal.com
-const String PAYPAL_CLIENT_ID = "AXhJHNQGq55tGByoYxgX7fZB4iRuPcJoiY3Dlf_nGHaXplM0Qs--DpOzRhwqOEpjIjeFVjd8kb9Cd02U";
+const String PAYPAL_CLIENT_ID = "PAYPAL ID";
 
 // your PayPal app's Client Secret, available from developer.paypal.com
-const String PAYPAL_CLIENT_SECRET = "EESj5ym8XdFkxjau14gy3mEyBlFEELqyP6mFpWdFzSYbfG0FqPoDgdUI_zKCv632JMxH3WG57n2JNbt9";
+const String PAYPAL_CLIENT_SECRET = "PAYPAL CLIENT SECRET";
 
 // a value to indicate whether you're using the PayPal API sandbox or not: 1 = yes, 0 = no.
 const String USE_SANDBOX = "1";
@@ -34,7 +34,7 @@ int maxRuns = 10;   // maximum number of times the Choreo should be executed
 
 void setup() {
   Serial.begin(9600);
-  
+
   // For debugging, wait until a serial console is connected.
   delay(4000);
   while(!Serial);
@@ -44,34 +44,34 @@ void loop()
 {
   if (numRuns <= maxRuns) {
     Serial.println("Running ListPayments - Run #" + String(numRuns++) + "\n");
-    
+
     TembooChoreo ListPaymentsChoreo;
 
     // invoke the Temboo client
     ListPaymentsChoreo.begin();
-    
+
     // set Temboo account credentials
     ListPaymentsChoreo.setAccountName(TEMBOO_ACCOUNT);
     ListPaymentsChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
     ListPaymentsChoreo.setAppKey(TEMBOO_APP_KEY);
-    
+
     // identify choreo to run
     ListPaymentsChoreo.setChoreo("/Library/PayPal/Payments/ListPayments");
-    
+
     // set choreo inputs
     ListPaymentsChoreo.addInput("ClientID", PAYPAL_CLIENT_ID);
     ListPaymentsChoreo.addInput("ClientSecret", PAYPAL_CLIENT_SECRET);
     ListPaymentsChoreo.addInput("UseSandbox", USE_SANDBOX);
 
-    
+
     // set choreo output filters
     ListPaymentsChoreo.addOutputFilter("time", "/payments/create_time", "Response");
     ListPaymentsChoreo.addOutputFilter("amount", "/payments/transactions/amount/total", "Response");
     ListPaymentsChoreo.addOutputFilter("id", "/payments/id", "Response");
-    
+
     // run choreo
     ListPaymentsChoreo.run();
-    
+
     // parse the results and print them to the serial monitor
     while(ListPaymentsChoreo.available()) {
         // read the name of the next output item
