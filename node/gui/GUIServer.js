0e588530c63b6a72757d818858c5d7a875fc7a98
Patric Lanhed <patriclanhed@gmail.com>
2015-10-17 09:36:02 +0200

    Changed import for hbs

--- node/gui/GUIServer.js
+++ node/gui/GUIServer.js
@@ -3,7 +3,7 @@
 const path = require('path');
 
 const express = require('express');
-const expressHbs = require('express3-handlebars');
+const expressHbs = require('express-handlebars');
 const sassMiddleware = require('node-sass-middleware');
 
 const baseConfig = {
@@ -36,7 +36,7 @@ module.exports = class GUIServer {
 	//
 	setupApp() {
 		let app = this.app;
-		
+
 		app.set('views', path.join(__dirname, 'views'));
 		app.engine('hbs',expressHbs({
 			extname:'hbs', 
