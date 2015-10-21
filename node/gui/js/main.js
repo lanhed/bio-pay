4b36d3fba0b3d8e67ef7857a8c0803b59197faab
Hektor Wallin <hektorw@gmail.com>
2015-10-21 14:24:42 +0200

    Adde som comments about gui flow

---
+++ node/gui/js/main.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/*
+
+App
+	screens:
+		start:
+			data:
+				exchange-rates?
+			render:
+				payment options
+				nav => new payment -> input-amount-screen
+			communication:
+				supported payments
+				exchange-rate
+			errors:
+				no exchange rate
+
+		input-amount:
+			data:
+				exchange-rate
+				currency
+				amount
+			render:
+				amount
+				exchanges
+				currency
+				keypad
+				nav => cancel -> start
+				nav => accept -> read-nfc
+			communication:
+				exchange-rate
+			errors:
+				exchange-rate
+
+		read-nfc:
+			data:
+				data type
+			render:
+				states:
+					reading
+					error
+					redo
+					done
+				logo
+				progress animation
+				nav => cancel -> start
+			communication:
+				read nfc
+			error:
+				no data of type
+				error reading
+				internal server error
+
+		process-payment:
+			data:
+				credentials
+				amount
+				currency
+			render:
+				status - animation
+				success
+				errors
+			communication:
+				make payment
+			errors:
+				payment error
+				internal server error
+
+ */
