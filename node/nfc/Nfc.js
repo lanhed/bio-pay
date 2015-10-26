b560e325b3a330e5296fbed20f1559190cb64066
Hektor Wallin <hektorw@gmail.com>
2015-10-26 21:04:53 +0100

    Testing serialport reader

--- node/nfc/Nfc.js
+++ node/nfc/Nfc.js
@@ -2,8 +2,7 @@
 
 // Debug
 const Debug = require('../utils/Debug');
-// https://github.com/voodootikigod/node-serialport
-// const SerialPort = require('serialport');
+const serialport = require('serialport');
 
 
 //
@@ -12,11 +11,72 @@ const Debug = require('../utils/Debug');
 const debugData = Debug.getJSON('nfc');
 
 
+const baseConfig = {
+	baudrate: 115200,
+	parserCharacter: '\n',
+	dataSeparator: ','
+};
+
+
 /**
  * Nfc
  */
 module.exports = class Nfc {
-	constructor() {}
+	constructor(config) {
+		this.config = Object.assign({}, baseConfig, config);
+
+		this.port = null;
+		this.connection = null;
+
+		this.setupSerialport();
+	}
+
+	setupSerialport() {
+		serialport.list((err, ports) => {
+			if (err) {
+				throw err;
+			}
+
+			let port = ports[0];
+			if (!port) {
+				throw 'Could not find any connected arduino';
+			}
+
+			this.setupConnection(port);
+		});
+	}
+
+	setupConnection(port) {
+		this.port = port;
+		let connection = this.connection = new serialport.SerialPort(port.comName, {
+			baudrate: this.config.baudrate,
+			parser: serialport.parsers.readline(this.config.parserCharacter)
+		});
+
+		connection.on('open', this.onOpen.bind(this));
+		connection.on('data', this.onData.bind(this));
+		connection.on('error', this.onError.bind(this));
+		connection.on('close', this.onClose.bind(this));
+	}
+
+	onOpen() {
+
+	}
+
+	onData(data) {
+	}
+
+	onError(error) {
+		console.log('Error from serialport');
+		console.error(error);
+	}
+
+	onClose() {
+
+	}
+
+
+
 
 	/**
 	 * read
